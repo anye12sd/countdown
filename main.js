7dd03bf913fbb5a9c3f04ca10ad5d5bfdfd455d2
vincent <541890086@qq.com>
2017-09-20 10:51:29 +0800

    first time

---
+++ main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by 55454 on 2017/9/20.
+ */
+$(function(){
+    $("#content .page").eq(4).siblings().css("display","none")
+    $(".timeText").val("2017/9/21 23:59:59")
+    $(".destroy").click()
+    $(".create").click()
+})
